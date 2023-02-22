fbca0adcd048ddacf598c7bd4b398312d74d8248
vickyrana321 <vivekrinayat321@gmail.com>
2023-02-22 23:33:45 +0530

    Making my first android app for Skyscanner VIP

---
+++ app/src/main/java/com/example/scannandroid/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.scannandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import net.skyscanner.backpack.card.BpkCardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
